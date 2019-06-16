44803afc1eca10e97f89b39ad6f89d43618c0c4d
Werria <verya.heydari@gmail.com>
2019-06-16 15:13:05 -0400

    Updates

---
+++ precache-manifest.eb2e0a27a6808c6d273ab4d48c419f56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "454184572360b3b0e598ceee4db27cf0",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "b2e3256f7b5676c23f34",
+    "url": "/my-resume/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "58e972d3f4b5f34cd8e7",
+    "url": "/my-resume/static/css/main.cd91f441.chunk.css"
+  },
+  {
+    "revision": "b2e3256f7b5676c23f34",
+    "url": "/my-resume/static/js/2.a2e31cd4.chunk.js"
+  },
+  {
+    "revision": "58e972d3f4b5f34cd8e7",
+    "url": "/my-resume/static/js/main.e4f5a83d.chunk.js"
+  },
+  {
+    "revision": "2f7b259ab1933d650523",
+    "url": "/my-resume/static/js/runtime~main.1423441b.js"
+  }
+]);
